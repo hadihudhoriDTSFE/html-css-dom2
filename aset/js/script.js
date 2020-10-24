bc8b7f79d5033354ba025a7866fd627f82b14978
hadihudhoriDTSFE <hadihudhoridts@gmail.com>
2020-10-24 09:27:34 +0700

    tugas

---
+++ aset/js/script.js
@@ -0,0 +1,28 @@
+function aktif1() {
+    const nav = document.getElementsByClassName("nav-link")
+    for(let i=0;i<4;i++) {
+        nav[i].classList.remove("text-danger")
+    }
+    nav[0].classList.add("text-danger")
+}
+function aktif2() {
+    const nav = document.getElementsByClassName("nav-link")
+    for(let i=0;i<4;i++) {
+        nav[i].classList.remove("text-danger")
+    }
+    nav[1].classList.add("text-danger")
+}
+function aktif3() {
+    const nav = document.getElementsByClassName("nav-link")
+    for(let i=0;i<4;i++) {
+        nav[i].classList.remove("text-danger")
+    }
+    nav[2].classList.add("text-danger")
+}
+function aktif4() {
+    const nav = document.getElementsByClassName("nav-link")
+    for(let i=0;i<4;i++) {
+        nav[i].classList.remove("text-danger")
+    }
+    nav[3].classList.add("text-danger")
+}
